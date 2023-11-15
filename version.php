38fa0c56b0b7c0a0b72bde7ed1ade0a6d3848ddf
Yedidia Klein <yedidia@openapp.co.il>
2023-11-15 15:52:55 +0200

    version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023111500;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2023111501;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2016052300;      // Requires this Moodle version.
 $plugin->component = 'repository_txttoimg';// Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.0 (Build - 2023111500)";
+$plugin->release = "1.0 (Build - 2023111501)";
